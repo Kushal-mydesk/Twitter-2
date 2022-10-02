4795fa565d93ebbfacc8d08be6d75e0adb90bbc5
Kushal Mondal <kushal99mondal@gmail.com>
2022-10-02 21:06:27 +0530

    Adding Error Handling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {SessionProvider} from 'next-auth/react'
-
+import ErrorBoundary from '../components/ErrorBoundary'
 function MyApp({ Component, pageProps:{session,...pageProps} }: any) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
     </SessionProvider>
   
   )
