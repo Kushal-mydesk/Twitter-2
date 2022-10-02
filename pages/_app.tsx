25a4f9e0a0035c5f434389571b4a424f43366ff6
Kushal Mondal <kushal99mondal@gmail.com>
2022-10-02 21:18:56 +0530

    Eslint Added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {SessionProvider} from 'next-auth/react'
-import ErrorBoundary from '../components/ErrorBoundary'
+
 function MyApp({ Component, pageProps:{session,...pageProps} }: any) {
   return (
     <SessionProvider session={session}>
-      <ErrorBoundary>
-        <Component {...pageProps} />
-      </ErrorBoundary>
+      
+      <Component {...pageProps} />
+      
     </SessionProvider>
   
   )
